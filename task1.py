d26c8be3e0bac0c3864a721e68a80034bdab19d8
Aayush <ayushkasera25@gmail.com>
2024-07-30 18:05:18 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,13 @@
+# temperature convert celsius to fahrenheit
+
+celsius = int(input("Enter the temperature in celsius:"))
+fahrenheit = (celsius * 9/5 ) + 32
+print("Temperature in fahrenheit is:", fahrenheit)
+
+
+
+# temperature convert celsius to kelvin
+celsius = int(input("Enter the temperature in celsius:"))
+kelvin = celsius + 273.15
+print("Temperature in kelvin is:", kelvin)
+
